febc13b78793935449148bed0afa2f69aff786c6
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-20 13:18:05 +0100

    Add component Posts and import it inside Home

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import Posts from '../../features/Posts/Posts';
 import styles from './Home.module.scss';
 import { Button } from 'react-bootstrap';
 
@@ -6,9 +7,12 @@ import { Button } from 'react-bootstrap';
 
 const Home = props => {
     return(
-        <div className={styles.homeHeader}>
-            <h2 className={styles.title}>All posts</h2>
-            <Button variant="outline-info">Add post</Button>
+        <div>
+            <div className={styles.homeHeader}>
+                <h2 className={styles.title}>All posts</h2>
+                <Button variant="outline-info">Add post</Button>
+            </div>
+            <Posts />
         </div>
     );
 };
