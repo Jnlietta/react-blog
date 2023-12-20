16a35b54947f7aeeaf3120513a2b73bcc25e4d0c
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-20 17:37:12 +0100

    Add margins to Home and Footer

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 
 const Footer = props => {
     return(
-        <p className={clsx('text-center', 'text-muted')}>Copyright © BlogApp 2022</p>
+        <p className={clsx('text-center', 'text-muted', 'mt-4', 'mb-4')}>Copyright © BlogApp 2022</p>
     );
 };
 
