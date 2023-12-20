f8e43a7d4f8f0f6477e3bf7fb295aeb86ac7fa32
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-20 14:19:40 +0100

    Add component Card to Posts and style it

--- src/components/features/Posts/Posts.js
+++ src/components/features/Posts/Posts.js
@@ -1,6 +1,28 @@
+import styles from "./Posts.module.scss";
+import { useSelector } from "react-redux";
+import { getAllPosts } from "../../../redux/postsRedux";
+import { Button, Card } from "react-bootstrap";
+
 const Posts = () => {
+    const posts = useSelector(getAllPosts);
+
     return (
-        <h3>Posts</h3>
+        <div>
+            {posts.map(post=>(
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title className={styles.title}>{post.title}</Card.Title>
+                        <Card.Text>
+                            <p className="mb-0"><span>Author: </span>{post.author}</p>
+                            <p className="mb-0"><span>Published: </span>{post.publishedDate}</p>
+                            <br />
+                            <p className="mb-0">{post.shortDescription}</p>
+                        </Card.Text>
+                        <Button variant="primary">Read more</Button>
+                    </Card.Body>
+                </Card>
+            ))}
+        </div>
     );
 };
 
