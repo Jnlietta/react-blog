82bb179cfa1388ab56c2d95364296eb42b385fe3
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-20 12:13:46 +0100

    Add Navbar from bootstrap and style it

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,8 +1,21 @@
 import styles from './NavBar.module.scss';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+
 
 const NavBar = props => {
     return(
-        <h1 className={styles.title}>NavBar</h1>
+        <Navbar bg="primary" data-bs-theme="dark" expand="lg" className="rounded">
+            <Container>
+                <Navbar.Brand href="#home" className={styles.brand}>Blog.app</Navbar.Brand>
+                <Nav className="justify-content-end">
+                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 };
 
