843bc1ac58a144dd4deb596ac11adbd40ba90b72
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-22 15:36:31 +0100

    Add component Post

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,8 +1,27 @@
+import { useSelector } from 'react-redux';
 import styles from './Post.module.scss';
+import { getAllPosts } from '../../../redux/postsRedux';
+import { Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
-const Post = props => {
+
+const Post = () => {
+const posts = useSelector(getAllPosts);
+
     return(
-        <h1 className={styles.title}>Post</h1>
+        <div className={styles.post}>
+            {posts.map(post=>(
+                <div>
+                    <h2 className={styles.title}>{post.title}</h2>
+                    <Button variant="outline-info" as={NavLink} to={"/post/edit/" + post.id}>Edit</Button>
+                    <Button variant="outline-danger">Delete</Button>
+                    <p className="mb-0"><span>Author: </span>{post.author}</p>
+                    <p className="mb-0"><span>Published: </span>{post.publishedDate}</p>
+                    <br />
+                    <p className="mb-0">{post.shortDescription}</p>
+                </div>
+            ))}
+        </div>
     );
 };
 
