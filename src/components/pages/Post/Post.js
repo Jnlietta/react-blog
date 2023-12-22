b0be850185fe48fd16618707b76fec54a5869c49
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-22 16:11:15 +0100

    Render article with same id as in path in Post

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -6,11 +6,13 @@ import { NavLink } from 'react-router-dom';
 
 
 const Post = () => {
-const posts = useSelector(state => getPostById(state, postId));
+    const idFromPath = window.location.pathname.split('/').filter(Boolean).pop();
+    //console.log('idFromPath', idFromPath);
+
+    const post = useSelector(state => getPostById(state, idFromPath));
 
     return(
         <div className={styles.post}>
-            {posts.map(post=>(
                 <div>
                     <h2 className={styles.title}>{post.title}</h2>
                     <Button variant="outline-info" as={NavLink} to={"/post/edit/" + post.id}>Edit</Button>
@@ -18,9 +20,8 @@ const posts = useSelector(state => getPostById(state, postId));
                     <p className="mb-0"><span>Author: </span>{post.author}</p>
                     <p className="mb-0"><span>Published: </span>{post.publishedDate}</p>
                     <br />
-                    <p className="mb-0">{post.shortDescription}</p>
+                    <p className="mb-0">{post.content}</p>
                 </div>
-            ))}
         </div>
     );
 };
