1d84ebb185f424661058b979359d01b271e5d13a
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-24 14:17:56 +0100

    Add event onChange to inputs of AddPostForm

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -12,23 +12,46 @@ const AddPostForm = () => {
         <Form>
             <Form.Group className="mb-3">
                 <Form.Label>Title</Form.Label>
-                <Form.Control placeholder="Enter title" />
+                <Form.Control 
+                    value={title} 
+                    onChange={e => setTitle(e.target.value)}
+                    placeholder="Enter title" 
+                    />
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Author</Form.Label>
-                <Form.Control placeholder="Enter author" />
+                <Form.Control 
+                    value={author} 
+                    onChange={e => setAuthor(e.target.value)}
+                    placeholder="Enter author" 
+                    />
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Published</Form.Label>
-                <Form.Control placeholder="dd-mm-yyyy"/>
+                <Form.Control 
+                    value={publishedDate} 
+                    onChange={e => setPublishedDate(e.target.value)}
+                    placeholder="dd-mm-yyyy" 
+                    />
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Short description</Form.Label>
-                <Form.Control placeholder="Leave a comment here" as="textarea" rows={3} />
+                <Form.Control 
+                    value={shortDescription} 
+                    onChange={e => setShortDescription(e.target.value)}
+                    placeholder="Leave a comment here" 
+                    as="textarea" 
+                    rows={3}
+                    />
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Main content</Form.Label>
-                <Form.Control placeholder="Leave a comment here" as="textarea" rows={10} />
+                <Form.Control 
+                    value={content} 
+                    onChange={e => setContent(e.target.value)}
+                    placeholder="Leave a comment here" 
+                    as="textarea" 
+                    rows={10} />
             </Form.Group>
 
             <Button variant="primary" type="submit">Add post</Button>
