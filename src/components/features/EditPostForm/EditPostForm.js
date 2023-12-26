11069af5348e6ab7ed3fef86c5d45b999ad802eb
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-26 21:30:34 +0100

    Use PostForm inside EditPostForm instead of Form, delate local state and change handleSubmit, get post id from path and post data

--- src/components/features/EditPostForm/EditPostForm.js
+++ src/components/features/EditPostForm/EditPostForm.js
@@ -1,78 +1,27 @@
-import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { addPost } from "../../../redux/postsRedux";
+import { useDispatch, useSelector } from "react-redux";
+import { editPost, getPostById } from "../../../redux/postsRedux";
 import { useNavigate } from "react-router-dom";
+import PostForm from "../PostForm/PostForm";
 
 const EditPostForm = () => {
-    const [title, setTitle] = useState ('');
-    const [author, setAuthor] = useState ('');
-    const [publishedDate, setPublishedDate] = useState ('');
-    const [shortDescription, setShortDescription] = useState ('');
-    const [content, setContent] = useState ('');
+
+    const idFromPath = window.location.pathname.split('/').filter(Boolean).pop();
+    const postToEdit = useSelector(state => getPostById(state, idFromPath));
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        dispatch(addPost({ title, author, publishedDate, shortDescription, content }));        
-        setTitle('');
-        setAuthor('');
-        setPublishedDate('');
-        setShortDescription('');
-        setContent('');
+    const handleSubmit = post => {
+        dispatch(editPost(post));
         navigate('/');
     }
 
     return(
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3">
-                <Form.Label>Title</Form.Label>
-                <Form.Control 
-                    value={title} 
-                    onChange={e => setTitle(e.target.value)}
-                    placeholder="Enter title" 
-                    />
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Author</Form.Label>
-                <Form.Control 
-                    value={author} 
-                    onChange={e => setAuthor(e.target.value)}
-                    placeholder="Enter author" 
-                    />
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Published</Form.Label>
-                <Form.Control 
-                    value={publishedDate} 
-                    onChange={e => setPublishedDate(e.target.value)}
-                    placeholder="dd-mm-yyyy" 
-                    />
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Short description</Form.Label>
-                <Form.Control 
-                    value={shortDescription} 
-                    onChange={e => setShortDescription(e.target.value)}
-                    placeholder="Leave a comment here" 
-                    as="textarea" 
-                    rows={3}
-                    />
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Main content</Form.Label>
-                <Form.Control 
-                    value={content} 
-                    onChange={e => setContent(e.target.value)}
-                    placeholder="Leave a comment here" 
-                    as="textarea" 
-                    rows={10} />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">Add post</Button>
-        </Form>
+        <PostForm
+            action={handleSubmit}
+            actionText="Add post"
+            {...postToEdit}
+            />
     );
 };
 
