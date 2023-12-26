fa0a7e622ad4a0676e754762e084a132b7ac132b
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-26 21:59:30 +0100

    Give as a payload for editPost copied post data and id from path, add Navigate to home page if there is no post with such id - all inside EditPostForm

--- src/components/features/EditPostForm/EditPostForm.js
+++ src/components/features/EditPostForm/EditPostForm.js
@@ -1,25 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
 import { editPost, getPostById } from "../../../redux/postsRedux";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import PostForm from "../PostForm/PostForm";
 
 const EditPostForm = () => {
 
-    const idFromPath = window.location.pathname.split('/').filter(Boolean).pop();
-    const postToEdit = useSelector(state => getPostById(state, idFromPath));
+    const id = window.location.pathname.split('/').filter(Boolean).pop();
+    const postToEdit = useSelector(state => getPostById(state, id));
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const handleSubmit = post => {
-        dispatch(editPost(post));
+        dispatch(editPost({...post, id}));
         navigate('/');
     }
+
+    if(!postToEdit) return <Navigate to="/" />
 
     return(
         <PostForm
             action={handleSubmit}
-            actionText="Add post"
+            actionText="Edit post"
             {...postToEdit}
             />
     );
