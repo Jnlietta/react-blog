a7715b5574624dcd1bb57eefcdb7b310558c8f04
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-02 00:25:16 +0100

    Add page Categories

---
+++ src/components/pages/Categories/Categories.js
@@ -0,0 +1,11 @@
+import styles from './Categories.module.scss';
+
+const Categories = props => {
+    return(
+        <div className={styles.container}>
+            <h2>All categories</h2>
+        </div>
+    );
+};
+
+export default Categories;
