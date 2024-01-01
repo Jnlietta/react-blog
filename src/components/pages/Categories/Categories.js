9b5b7e625fd82114998cd0943c15bb27c844ca5a
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-02 00:42:22 +0100

    Add Card and NavLink inside Categories

--- src/components/pages/Categories/Categories.js
+++ src/components/pages/Categories/Categories.js
@@ -1,9 +1,24 @@
+import { Card, ListGroup } from 'react-bootstrap';
 import styles from './Categories.module.scss';
+import { useSelector } from 'react-redux';
+import { getAllCategories } from '../../../redux/categoriesRedux';
+import { NavLink } from 'react-router-dom';
 
 const Categories = props => {
+    const categories = useSelector(getAllCategories);
+
     return(
         <div className={styles.container}>
-            <h2>All categories</h2>
+            <h2 className={styles.header}>All categories</h2>
+            <Card>
+                <ListGroup variant="flush">
+                    {categories.map(category=>(
+                        <ListGroup.Item>
+                            <NavLink as={NavLink} to="/about">{category.name}</NavLink>
+                        </ListGroup.Item>
+                        ))}
+                </ListGroup>
+            </Card>
         </div>
     );
 };
